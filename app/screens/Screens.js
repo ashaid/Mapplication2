38f80a589c80fadebbd1bcc105180e17d4b18ebd
ashaid <ashaid2@lsu.edu>
2021-11-18 02:29:57 +0000

    fixed background Image and centered test buttons

--- app/screens/Screens.js
+++ app/screens/Screens.js
@@ -1,34 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, ImageBackground, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import { AuthContext } from "../components/AuthContext";
 import { ScreenContainer } from "../components/ScreenContainer";
-
-
-const styles = StyleSheet.create({
-
-  button: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 10,
-    borderRadius: 5,
-  },
-  imgBackground: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-  },
-});
+import { Style, Colors } from "../style/styles";
+import { Wobble } from "../components/LogoAnimation";
 
 export const Home = ({ navigation }) => (
   <ScreenContainer>
     <ImageBackground
-      style={styles.imgBackground}
+      style={{ flex: 1, width: "100%", height: "100%" }}
       resizeMode="cover"
       source={require("../assets/Background.png")}
-    ></ImageBackground>
+    >
+      <Text style={{ color: Colors.white }}>Hi</Text>
+    </ImageBackground>
   </ScreenContainer>
 );
 
@@ -41,17 +33,11 @@ export const Splash = () => (
 export const LogIn = ({ navigation }) => {
   const { logIn } = React.useContext(AuthContext);
 
-  return (  
-    <ScreenContainer>
-      <ImageBackground
-        style={styles.imgBackground}
-        resizeMode="cover"
-        source={require("../assets/Background.png")}
-      >
-          <Button  
-          title = "Log In"
-          onPress = {() => logIn()}
-          />
+  return (
+    <ScreenContainer style={{ flex: 1 }}>
+      <ImageBackground style={styles.background}>
+        <Wobble />
+        <Button title="Log In" onPress={() => logIn()} />
       </ImageBackground>
     </ScreenContainer>
   );
@@ -59,16 +45,42 @@ export const LogIn = ({ navigation }) => {
 
 export const ProfileScreen = ({ navigation }) => {
   return (
-    <ScreenContainer >
+    <ScreenContainer>
       <ImageBackground
-        style={styles.imgBackground}
+        style={{ flex: 1, width: "100%", height: "100%" }}
         resizeMode="cover"
         source={require("../assets/Background.png")}
       >
-        <Button title="Find My Classes" />
-        <Button title="My Maps" />
-        <Button title="UR mom lol" />
+        <View style={Style.centerItem}>
+          <Button title="Find My Classes" />
+          <Button title="My Maps" />
+          <Button title="UR mom lol" />
+        </View>
       </ImageBackground>
     </ScreenContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+  imgBackground: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: Colors.tertiary,
+    width: "100%",
+    height: "100%",
+  },
+});
