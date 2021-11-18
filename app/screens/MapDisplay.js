baaa7c9b70eca0a06dd73e8ee41d485c2df2147d
bdcalato <bcalat1@lsu.edu>
2021-11-17 22:24:30 -0600

    Skeleton of Map Display UI has been made, still need dropdowns for building names, can't find good solution...

--- app/screens/MapDisplay.js
+++ app/screens/MapDisplay.js
@@ -8,8 +8,11 @@ import {
   TextInput,
   useState,
   Button,
+  Alert,
 } from "react-native";
 import { Style, Colors } from "../style/styles";
+
+
 
 class MapDisplay extends Component {
   //const [startingRoom, setStartingRoomText] = React.useState("");
@@ -19,9 +22,55 @@ class MapDisplay extends Component {
 
     this.state = {};
   }
+
   render() {
-    return <MapDisplayComponent />;
+    //NEED DROP DOWN MENU SOLUTION FOR BUILDINGS, CANNOT RELY ON USER INPUT FOR BUILDINGS TOO EASY FOR SPELLING ERRORS
+    return (
+      <View style={styles.container}>
+        <View>
+          <Text>Enter Starting Point:</Text>
+        </View>
+        <View>
+          <TextInput 
+            style={styles.input}
+            placeholder={'Ex: 1200'}
+            maxLength= {4}
+          />
+        </View>
+        <View>
+          <Text>Enter Destination:</Text>
+        </View>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder={'Ex: 1615'}
+            maxLength = {4}
+          />
+        </View>
+
+        <Button title="Submit"
+        onPress={() => Alert.alert('Simple Button Pressed')/*collects user input and sends to API call, then display the actual maps to the user*/}
+        />
+
+      </View>
+    );
   }
 }
 
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#777',
+      padding: 8,
+      margin: 10,
+      width: 70,
+    }
+  });
 export default MapDisplay;
