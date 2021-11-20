b4f1c62fb111480161adc9b649d165a37e565c65
bdcalato <bcalat1@lsu.edu>
2021-11-19 18:28:46 -0600

    I have done a few image carousels in my day

--- app/screens/subscreens/MapViews.js
+++ app/screens/subscreens/MapViews.js
@@ -1,37 +1,60 @@
-import React, {Component} from "react";
+import React, { useState, useRef, Component} from "react";
 import "react-native-reanimated";
 import {
-  Image,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    TouchableOpacity,
+    View,
+    Text,
+    Image,
+    FlatList,
+    Dimensions
 } from "react-native";
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
-function MapViews(props){
+const { width } = Dimensions.get('window');
+const SPACING = 10;
+const THUMb_SIZE = 80;
+
+const IMAGES = {
+    image1: require('../../assets/images/1.png'),
+    image2: require('../../assets/images/2.png'),
+}
+
+export default class MapViews
+{
+    render(){
+        const [images, setImages] = useState([
+            { id: '1', image: IMAGES.image1 },
+            { id: '2', image: IMAGES.image2 }, 
+        ]);
     return(
-
-        <ImageBackground style = {styles.background}></ImageBackground>
-
-
-    );
+        <View style={{flex: 1, backgroundColor: 'black', alignItems: 'center'}}>
+            <Text style={{color: 'white', fontSize: 32, marginTop: 50, marginBottom: 25}}>
+                Map Viewer
+            </Text>
+            <View style={{flex: 1 / 2, marginTop: 20}}>
+                <Carousel
+                    layout='default'
+                    data={images}
+                    sliderWidth={width}
+                    itemWidth={width}
+                    renderItem={({ item, index })=>(
+                        <Image
+                            key={index}
+                            style={{ width: '100% ', height: '100%' }}
+                            resizeMode='contain'
+                            source={item.image}
+                        />
+                    )
+                
+                }
+                />
+            </View>
+        </View>
+    );    
+ }
 }
 
 const styles = StyleSheet.create({
 
-    background: {
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-        backgroundColor: "#131313",
-
-        justifyContent: "space-between",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        padding: 10,
-    },
 
 })
-
-export default MapViews;
