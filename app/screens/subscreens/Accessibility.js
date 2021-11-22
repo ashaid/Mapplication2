384ed5ac91f7ce4e8eb69590c9b9120b85e77ae8
Doof <jdufrechou123@gmail.com>
2021-11-22 16:01:47 -0600

    Changes to Accessibility spelling and spacing

--- app/screens/subscreens/Accessibility.js
+++ app/screens/subscreens/Accessibility.js
@@ -14,12 +14,12 @@ export class Accessibility extends Component{
         return(
             <ImageBackground style = {styles.background}>
                 <Text style = {styles.TitleText}>
-                Mapplication is accessiable to all users {"\n"}
+                Mapplication is accessible to all users {"\n"}
                 </Text>
                  <Text style = {styles.TermsText}>
                     We complete this by {"\n"}
-                    1) Having the app available for all ios and andriod users
-                    2) Allowing all functions for the app to be accessiable 
+                    1) Having the app available for all ios and android users {"\n"}
+                    2) Allowing all functions for the app to be accessible 
                 </Text>
             </ImageBackground>
         );
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
         position: "relative",
         color: "white",
         fontWeight: "bold",
-        fontSize: 65,
+        fontSize: 55,
         textAlign: "center",
     }
 })
