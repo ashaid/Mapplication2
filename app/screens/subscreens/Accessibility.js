f0a1a48dfbaec651176f73b8651e964ce6123203
Joshua <rovira.josh@gmail.com>
2021-11-22 16:09:05 -0600

    fixed accessibility, fixed map display 'go' button

--- app/screens/subscreens/Accessibility.js
+++ app/screens/subscreens/Accessibility.js
@@ -7,20 +7,36 @@ import {
   Text,
   View,
 } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 export class Accessibility extends Component{
     render(){
         return(
             <ImageBackground style = {styles.background}>
-                <Text style = {styles.TitleText}>
-                Mapplication is accessiable to all users {"\n"}
-                </Text>
-                 <Text style = {styles.TermsText}>
-                    We complete this by {"\n"}
-                    1) Having the app available for all ios and andriod users
-                    2) Allowing all functions for the app to be accessiable 
-                </Text>
+                <View style = {styles.headerContainer}>
+                    <Text style = {styles.titleText}>
+                        Options
+                    </Text>
+                </View>
+                <View style = {styles.line}></View>
+                <View style = {styles.buttonContainer}>
+                <TouchableOpacity style = {styles.languageContainer}>
+                    <Text style = {styles.buttonText}>
+                        Select Language
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style = {styles.dictationContainer}>
+                    <Text style = {styles.buttonText}>
+                        Enable Dictation
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style = {styles.ADAContainer}>
+                    <Text style = {styles.buttonText}>
+                        Enable ADA Directions
+                    </Text>
+                </TouchableOpacity>
+                </View>
             </ImageBackground>
         );
        }
@@ -28,27 +44,71 @@ export class Accessibility extends Component{
 
 const styles = StyleSheet.create({
     background: {
-        color: "#131313",
         backgroundColor: "#131313",
         flex: 1,
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
     },
-    TermsText: {
-        display: "flex",
-        position: "relative",
-        color: "#F7FAF8",
-        fontWeight: "bold",
-        fontSize: 35,
-        textAlign: "left",
-        alignSelf: "auto",
+    headerContainer: {
+      width: "100%",
+      alignItems: "center",
+      justifyContent: "center",
+      top: 7,
+      paddingHorizontal: 30,
+      borderRadius: 10,
     },
-    TitleText: {
-        display: "flex",
-        position: "relative",
+    buttonContainer: {
+        flex: .5,
+    },
+    languageContainer: {
+      backgroundColor: "#c2f043",
+      height: "60%",
+      width: "100%",
+      borderRadius: 10,
+      justifyContent: "center",
+      alignItems: "center",
+      paddingHorizontal: 60,
+    },
+    dictationContainer: {
+        backgroundColor: "#61c924",
+        height: "60%",
+        width: "100%",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 60,
+    },
+    ADAContainer: {
+      backgroundColor: "#1f820d",
+      height: "60%",
+      width: "100%",
+      borderRadius: 10,
+      justifyContent: "center",
+      alignItems: "center",
+      paddingHorizontal: 35,
+    },
+    buttonText: {
         color: "white",
         fontWeight: "bold",
-        fontSize: 65,
+        fontSize: 20,
         textAlign: "center",
-    }
+    },
+    titleText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 45,
+    },
+    line: {
+        color: "white",
+        backgroundColor: "white",
+        width: "75%",
+        height: 2,
+        display: "flex",
+        flexDirection: "row",
+        marginVertical: 10,
+        borderRadius: 5,
+    },
 })
 
 export default Accessibility;
